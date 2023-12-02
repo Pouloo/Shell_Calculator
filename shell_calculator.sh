dac84d85bd8c9f05d10a1ae18e501c6a198e7679
Paul Sicoli <paul.sicoli@outlook.com>
2023-12-02 19:39:10 +0100

    Quicksave!

---
+++ shell_calculator.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+CONT=1;
+while [ $CONT == 1 ]
+do
+	clear
+
+	echo "Choose type of operation"
+	echo "1: Addition"
+	echo "2: Substraction"
+	echo "3: Multiplication"
+	echo "4: Divison"
+	echo "5: Division (Mod)"
+	read OP
+	if [ $OP -gt 5 ]
+	then
+		echo "Invalid input"
+		sleep 0.5
+		continue
+	fi
+	echo "Choose number 1"
+	read A
+	echo "Choose number 2"
+	read B
+	echo -e '\n'
+	echo "Result:"
+	if [ $OP == 1 ]
+	then
+		echo `expr $A + $B`
+	elif [ $OP == 2 ]
+	then
+		echo `expr $A - $B`
+	elif [ $OP == 3 ]
+	then
+		echo `expr $A \* $B`
+	elif [ $OP == 4 ]
+	then
+		echo `expr $A / $B`
+	elif [ $OP == 5 ]
+	then
+		echo `expr $A % $B`
+	fi
+	echo -e '\n'
+	echo "Continue with another operation?" 
+	echo "1: Continue" 
+	echo "0: End" 
+	read CONT
+done
